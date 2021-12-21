5fe7fa8e12637a2a3dd68980749748aa74647629
Mahs_kz07@itstep.edu.az <Mahs_kz07@itstep.edu.az>
2021-12-21 12:00:32 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             List<Socket> sockets = new List<Socket>();
+            var ipadress = IPAddress.Parse("10.1.18.42");
+            int port = 27001;
+            using (var socket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp))
+            {
+                var ep = new IPEndPoint(ipadress, port);
+                socket.Bind(ep);
+
+                socket.Listen(10);
+                Console.WriteLine($"Listening  on {socket.LocalEndPoint}");
+
+
+                while (true)
+                {
+                    var client = socket.Accept();
+                    
+                    Task.Run(() => {
+
+                        Console.WriteLine($"{client.RemoteEndPoint} connected  . . . .");
+
+                        var lenght = 0;
+                        var bytes = new byte[1024];
+
+                        do
+                        {
+                            lenght = client.Receive(bytes);
+                            var msg = Encoding.UTF8.GetString(bytes, 0, lenght);
+                            Console.WriteLine($"Client : {client.RemoteEndPoint} : {msg}");
+                            if (msg == "ok") {
+
+                                client.Shutdown(SocketShutdown.Both);
+                                client.Dispose();
+                                break;
+                            
+                            }
+                        } while (true);
+
+                        
+
+
+                    });
+                   
+
+                }
+            }
+        }
+    }
+}
---
+++ TCPClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    class Program
+    {
+           static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var ipAdress = IPAddress.Parse("10.1.18.42");
+            var port = 27001;
+
+            var ep = new IPEndPoint(ipAdress, port);
+
+            try
+            {
+                socket.Connect(ep);
+
+                if (socket.Connected) {
+                    Console.WriteLine("Connected to the Server . . . .");
+                
+                }
+                else
+                {
+                    Console.WriteLine("Can not Connected to the Server");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can Not connect to the Server");
+                Console.WriteLine(ex.Message);
+            }
+
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                var bytes = Encoding.UTF8.GetBytes(msg);
+                socket.Send(bytes);
+            }
+
+           
+        }
+    }
+}
